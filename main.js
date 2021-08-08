a077dcd7b102cf4464ea8118ff9cedd2d2b7f02f
Gleb Medvedev <fredwantblossom@gmail.com>
2021-08-08 17:02:52 +0300

    fix filter functions on

--- main.js
+++ main.js
@@ -1,18 +1,17 @@
 let goodsList = [];
-
 let counter = 1;
-
-let $subtitle = document.querySelector('.subtitle');
+let toggleStatus = 0;
+let $ol = document.querySelector('.goods-list');
 let $allBoughtButton = document.querySelector('.btn-allBought');
 let $boughtButton = document.querySelector('.btn-bought');
-let $plannedButton = document.querySelector('.btn-plannedBought')
+let $plannedButton = document.querySelector('.btn-plannedBought');
 let $paramTitle = document.querySelector('#filter-param');
 let $sortingParam = document.querySelector('#sorting-param');
-let $ol = document.querySelector('.goods-list');
 let $name = document.getElementById('name');
 let $count = document.getElementById('count');
 let $price = document.getElementById('price');
 let $date = document.getElementById('date');
+let $subtitle = document.querySelector('.subtitle');
 let $filterLength = document.querySelector('.length');
 
 document.getElementById('add').addEventListener('click', function() {
@@ -22,12 +21,13 @@ document.getElementById('add').addEventListener('click', function() {
       count: $count.value || 'Не задано',
       price: $price.value || 'Не задано',
       date: $date.value || 'Не задано',
-      bought: false
+      bought: false,
     };
 
     function identityCheck() {
       return goodsList.every(
-          (item) => JSON.stringify(item.name) !== JSON.stringify(goodsListItem.name)
+          (item) =>
+              JSON.stringify(item.name) !== JSON.stringify(goodsListItem.name)
       );
     }
 
@@ -38,13 +38,13 @@ document.getElementById('add').addEventListener('click', function() {
       $count.value = '';
       $price.value = '';
       $date.value = '';
-
     } else {
       alert('Нельзя добавить уже существующий товар');
     }
   } else {
     alert('Нельзя добавить товар без наименования');
   }
+  $subtitle.innerText = 'Все покупки.';
   $filterLength.innerText = `Найдено: ${goodsList.length} продуктов.`;
 });
 
@@ -58,101 +58,148 @@ function paintNewItem(item) {
         <li>Количество: ${item.count}</li>
         <li>Цена: ${item.price}</li>
         <li>Дата покупки: ${item.date}</li>
-        <li>${item.bought ? "Куплено" : 'Покупка планируется'}</li>
+        <li>${item.bought ? 'Куплено' : 'Покупка планируется'}</li>
       </ul>
     </li>
 `;
   $ol.insertAdjacentHTML('beforeend', itemData);
 
+  let $checkbox = document.getElementById(`checkbox-${counter}`);
+  let $checkboxParent = $checkbox.parentNode;
+
   function changeCheck() {
-    let $checkbox = document.getElementById(`checkbox-${counter}`);
     $checkbox.addEventListener('click', () => {
       if ($checkbox.checked) {
-        $checkbox.parentNode.classList.add('checked-product')
+        $checkboxParent.classList.add('checked-product');
         item.bought = true;
-        console.log(item)
       } else {
-        $checkbox.parentNode.classList.remove('checked-product')
+        $checkboxParent.classList.remove('checked-product');
         item.bought = false;
-        console.log(item)
       }
-    })
+    });
   }
 
-  changeCheck()
   counter++;
+  changeCheck();
 }
 
-//  paint bought item
-const paintBoughtItems = list => {
-  $ol.innerHTML = '';
-  list.forEach(item => paintNewItem(item))
+if (toggleStatus == 0) {
+  $paramTitle.addEventListener('keyup', () => {
+    let filteredList = [];
+    goodsList.forEach((item) => {
+      let value = item.name.toLowerCase();
+      let filterInput = $paramTitle.value.toLowerCase();
+      if (value.includes(filterInput)) {
+        filteredList.push(item);
+      }
+    });
+    $filterLength.innerText = `Найдено: ${filteredList.length} продуктов.`;
+    paintNewList(filteredList);
+  });
+}
 
+//  paint all items
+const paintAllItems = (list) => {
+  $ol.innerHTML = '';
+  list.forEach((item) => {
+    paintNewItem(item);
+  });
 };
 
+$allBoughtButton.addEventListener('click', () => {
+  toggleStatus = 1;
+  paintAllItems(goodsList);
+  $subtitle.innerText = 'Все покупки.';
+  $filterLength.innerText = `Найдено: ${goodsList.length} продуктов.`;
+  if (toggleStatus === 1) {
+    $paramTitle.addEventListener('keyup', () => {
+      let filteredList = [];
+      goodsList.forEach((item) => {
+        let value = item.name.toLowerCase();
+        let filterInput = $paramTitle.value.toLowerCase();
+        if (value.includes(filterInput)) {
+          filteredList.push(item);
+        }
+      });
+      $filterLength.innerText = `Найдено: ${filteredList.length} продуктов.`;
+      paintNewList(filteredList);
+    });
+  }
+  console.log(goodsList)
+});
+//  paint all items
+
+//  paint bought item
+const paintBoughtItems = (list) => {
+  $ol.innerHTML = '';
+  list.forEach((item) => paintNewItem(item));
+};
+
+let boughtItems;
 $boughtButton.addEventListener('click', () => {
-  let boughtItems = [];
+  toggleStatus = 2;
+  boughtItems = [];
   goodsList.forEach((item) => {
     if (item.bought) {
       boughtItems.push(item);
     }
-  })
+  });
   paintBoughtItems(boughtItems);
   $subtitle.innerText = 'Купленные.';
   $filterLength.innerText = `Найдено: ${boughtItems.length} продуктов.`;
-})
-
+  if (toggleStatus === 2) {
+    $paramTitle.addEventListener('keyup', () => {
+      let filteredList = [];
+      boughtItems.forEach((item) => {
+        let value = item.name.toLowerCase();
+        let filterInput = $paramTitle.value.toLowerCase();
+        if (value.includes(filterInput)) {
+          filteredList.push(item);
+        }
+      });
+      $filterLength.innerText = `Найдено: ${filteredList.length} продуктов.`;
+      paintNewList(filteredList);
+    });
+  }
+  console.log(boughtItems)
+});
 //  paint bought item
 
 //  paint planned bought
-const paintPlannedItems = list => {
+const paintPlannedItems = (list) => {
   $ol.innerHTML = '';
-  list.forEach(item => paintNewItem(item));
+  list.forEach((item) => paintNewItem(item));
 };
 
+let plannedItems;
 $plannedButton.addEventListener('click', () => {
-  let plannedItems = [];
+  toggleStatus = 3;
+  plannedItems = [];
   goodsList.forEach((item) => {
-    if (item.bought === false) {
+    if (!item.bought) {
       plannedItems.push(item);
     }
-  })
+  });
   paintPlannedItems(plannedItems);
   $subtitle.innerText = 'Планируемые покупки.';
   $filterLength.innerText = `Найдено: ${plannedItems.length} продуктов.`;
-})
-
-//  paint planned bought
-
-//  paint all items
-const paintAllItems = list => {
-  $ol.innerHTML = '';
-  list.forEach(item => paintNewItem(item));
-};
-
-$allBoughtButton.addEventListener('click', () => {
-  paintAllItems(goodsList);
-  $subtitle.innerText = 'Все покупки.';
-  $filterLength.innerText = `Найдено: ${goodsList.length} продуктов.`;
-})
-//  paint all items
-
-
-// filter TODO:
-$paramTitle.addEventListener('keyup', () => {
-  let filteredList = [];
-  goodsList.forEach((item) => {
-    let value = item.name.toLowerCase();
-    let filterInput = $paramTitle.value.toLowerCase();
-    if (value.includes(filterInput)) {
-      filteredList.push(item);
-    }
-  });
-  $subtitle.innerText = 'Все покупки.';
-  $filterLength.innerText = `Найдено: ${filteredList.length} продуктов.`;
-  paintNewList(filteredList);
+  if (toggleStatus === 3) {
+    $paramTitle.addEventListener('keyup', () => {
+      let filteredList = [];
+      plannedItems.forEach((item) => {
+        let value = item.name.toLowerCase();
+        let filterInput = $paramTitle.value.toLowerCase();
+        if (value.includes(filterInput)) {
+          filteredList.push(item);
+        }
+      });
+      $filterLength.innerText = `Найдено: ${filteredList.length} продуктов.`;
+      paintNewList(filteredList);
+    });
+  }
 });
 
+//  paint planned bought
 function paintNewList(list) {
   $ol.innerHTML = '';
   list.forEach((item) => {
@@ -188,3 +235,6 @@ function clearList() {
     goodsList = [];
   }
 }
+
+
+
